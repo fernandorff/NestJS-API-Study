2fb0e52ea406f274927e77d2b9520dbe237b5609
Fernando Rocha <fernandorff92@gmail.com>
2023-09-29 18:06:34 -0300

    .

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,9 +1,12 @@
 import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsEmail()
+  @ApiProperty({ example: 'user@example.com', description: 'User email' })
   email: string;
 
   @IsString()
+  @ApiProperty({ example: 'password123', description: 'User password' })
   password: string;
 }
--- src/users/dtos/update-user.dto.ts
+++ src/users/dtos/update-user.dto.ts
@@ -1,11 +1,14 @@
 import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
   @IsEmail()
   @IsOptional()
+  @ApiProperty({ example: 'user@example.com', description: 'User email' })
   email: string;
 
   @IsString()
   @IsOptional()
+  @ApiProperty({ example: 'password123', description: 'User password' })
   password: string;
 }
